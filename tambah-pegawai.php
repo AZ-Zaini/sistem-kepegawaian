a0c312b81ab5e7a4d28e73725bc3477c7049a2b6
az-zaini <akhmadjaini97@gmail.com>
2026-07-21 15:26:29 +0800

    commit

--- tambah-pegawai.php
+++ tambah-pegawai.php
@@ -3,7 +3,6 @@ session_start();
 require_once 'koneksi.php';
 require_once 'auth.php';
 isLogin();
-cek_role(['admin']);
 
 if (isset($_POST['simpan'])) {
     $nip = mysqli_real_escape_string($koneksi, $_POST['nip']);
